348e416b930830eedc1a0d6b4cdb763e3ba31bbc
conormcmahon <conormcamhon22@gmail.com>
2021-10-19 12:50:10 -0700

    Changed to run download as a subprocess to ensure that loop continues through whole list even if an error occurs

--- landsat_download.py
+++ landsat_download.py
@@ -2,6 +2,7 @@ import json
 from landsatxplore.api import API
 from landsatxplore.earthexplorer import EarthExplorer
 import argparse
+import subprocess
 
 # user input    
 parser = argparse.ArgumentParser()
@@ -12,6 +13,7 @@ parser.add_argument('-lo', '--longitude', required=True, help='Longitude')
 parser.add_argument('-ds', '--start', required=True, help='Start Date')
 parser.add_argument('-de', '--end', required=True, help='End Date')
 parser.add_argument('-d', '--dataset', required=True, help='Dataset')
+parser.add_argument('-c', '--cloud', required=True, help='Max Cloud Cover')
 
 args = parser.parse_args()
 
@@ -21,10 +23,11 @@ latitude = args.latitude
 longitude = args.longitude     
 start = args.start
 end = args.end     
-dataset = args.dataset     
+dataset = args.dataset    
+cloud_max = args.cloud     
 
 print("\nStarting request to download data from " + dataset + " from " + start + " to " + end)
-print("  Data will be spatially limited between " + latitude + " N and " + longitude + " E.")
+print("  Data will be spatially limited between " + latitude + " N and " + longitude + " E. No scenes with more than " + cloud_max + " percent cloud cover.")
 
 # Initialize a new API instance and get an access key
 api = API(username, password)
@@ -36,7 +39,7 @@ scenes = api.search(
     longitude=float(longitude),
     start_date=start,
     end_date=end,
-    max_cloud_cover=10
+    max_cloud_cover=cloud_max
 )
 
 print(f"{len(scenes)} scenes found.")
@@ -51,6 +54,37 @@ api.logout()
 ee = EarthExplorer(username, password)
 for scene in scenes:
     print("Downloading a scene from " + scene['acquisition_date'].strftime('%Y-%m-%d') + " with ID " + scene['entity_id'])
-    ee.download(scene['entity_id'], output_dir='./data', dataset=dataset)
+    # The below command uses the native python downloader in LandsatXPlore, but if it throws an error the whole process will break
+    #ee.download(scene['entity_id'], output_dir='./data', dataset=dataset)
+    # This runs the commandline utility for LandsatXPlore in a subprocess so that if it errors out, more scenes can still be downloaded
+    subprocess.run(['landsatxplore', 'download', scene['entity_id'], '-d', dataset, '-u', username, '-p', password])
+    # This hackily directly downloads the scene, circumventing problems that LandsatXPlore is having with donwloading Landsat Collection 2 Level 2 imagery (as of Oct. 19 2021)
+    #    check this for more info... I attempted these workarounds, without success: https://github.com/yannforget/landsatxplore/issues/42
+    #    This will hard-code an attempt to download from the Landsat 8 C2-L2 data product ID 5e83d14fec7cae84. May need to use others for certain dates? 
+    #    Took the download code from this: https://stackoverflow.com/questions/22676/how-to-download-a-file-over-http
+#    url = " https://earthexplorer.usgs.gov/download/5e83d14fec7cae84/" + scene['entity_id'] + "/EE/"
+#    print("Attempting to directly download from " + url)
+#    file_name = url.split('/')[-1]
+#    u = urllib2.urlopen(url)
+#    f = open(file_name, 'wb')
+#    meta = u.info()
+#    file_size = int(meta.getheaders("Content-Length")[0])
+#    #print "Downloading: %s Bytes: %s" % (file_name, file_size)
+#
+#    file_size_dl = 0
+#    block_sz = 8192
+#    while True:
+#        buffer = u.read(block_sz)
+#        if not buffer:
+#            break
+#
+#        file_size_dl += len(buffer)
+#        f.write(buffer)
+#        status = r"%10d  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
+#        status = status + chr(8)*(len(status)+1)
+#        print(status),
+#
+#    f.close()
+
 
 ee.logout()
